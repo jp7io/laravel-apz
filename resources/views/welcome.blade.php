4aaa3ab96361fec0f0b105c2bca36f42909a7452
Rafael Testa <rafael@jp7.com.br>
2015-07-17 15:29:46 -0300

    Add welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends($template)
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+    <div class="jumbotron">
+        <h1>Laravel Apz</h1>
+        <p>The guide to build a Laravel app from a to z.</p>
+        <p>
+            <a href="https://github.com/jp7internet/laravel-apz" role="button" class="btn btn-lg btn-primary">View On GitHub</a>
+        </p>
+    </div>
+@endsection
