cc9896ec242c6df7e10089b306de9480ebc10df6
JP Barbosa <jp@jp7.com.br>
2016-03-11 18:27:52 -0300

    Ensure correct articles form is being tested

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,7 +1,7 @@
 @extends($template)
 
 @section('content')
-  {!! Form::open(['route' => 'articles.store', 'data-remote' => $remote]) !!}
+  {!! Form::open(['route' => 'articles.store', 'data-remote' => $remote, 'id' => 'articles-form']) !!}
       @include ('articles.form', ['submitButtonText' => 'Add Article'])
   {!! Form::close() !!}
 @endsection
