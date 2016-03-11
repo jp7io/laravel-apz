4b0d3ce7df4e051e09450b01233b55beb64c0eaa
JP Barbosa <jp@jp7.com.br>
2016-03-11 18:26:09 -0300

    Fix Open Weather API key

--- app/WeatherApi.php
+++ app/WeatherApi.php
@@ -14,10 +14,12 @@ class WeatherApi
         return Cache::get('Weather.reads') ? $data: $this->update();
     }
 
-    public function update($urlApi = 'http://api.openweathermap.org/data/2.5/weather?q=Sao_Paulo,br&units=metric')
+    public function update($urlApi = null)
     {
-        $urlApi .= ('&APPID=' . getenv('WEATHER_APPID'));
-
+        if (is_null($urlApi)) {
+            $urlApi = 'http://api.openweathermap.org/data/2.5/weather?q=Sao_Paulo,br&units=metric&APPID=' . getenv('WEATHER_APPID');
+        }
+        
         $json = file_get_contents($urlApi);
         $response = json_decode($json);
         $data = $response->main;
